78c19322a4b6074dd814b088a34627bf97dee401
Viktor-sun <67188617+Viktor-sun@users.noreply.github.com>
2020-10-12 14:14:18 +0300

    Update task6.js

--- js/task6.js
+++ js/task6.js
@@ -13,3 +13,17 @@ console.log(mapArray([-2, 0, 2]));
 
 //console.log(mapArray([-2.5, 0, 2.5]));
 // [-25, 0, 25]
+
+
+// второй вариант решения
+// function mapArray(array) {
+//   'use strict';
+//   const numbers = new Array(array.length);
+//   for(let i = 0; i < array.length; i += 1) {
+//     // Write code under this line
+//    numbers[i] = array[i] * 10;
+   
+    
+//   }
+//   return numbers;
+// }
